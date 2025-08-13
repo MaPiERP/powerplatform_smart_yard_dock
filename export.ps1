30f41188e33ef76f802b8106148ba884885c6b13
MaPiERP <88823266+MaPiERP@users.noreply.github.com>
2025-08-13 11:46:18 +0200

    release: v0.1.0 export & unpack from DEV

---
+++ export.ps1
@@ -0,0 +1,6 @@
+﻿param(
+  [string]$EnvUrl   = "https://org4d258410.crm4.dynamics.com/",
+  [string]$Solution = "SmartYardDock",
+  [string]$RepoRoot = (Resolve-Path .)
+)
+& ".\scripts\Export-Unpack.ps1" -EnvUrl $EnvUrl -Solution $Solution -RepoRoot $RepoRoot
